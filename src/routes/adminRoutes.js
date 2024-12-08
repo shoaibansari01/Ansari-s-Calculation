fb07e5deac756e721233e2e247064a6164599513
Shoaib Ansari <shoaib.ansari8600@gmail.com>
2024-12-08 15:29:13 +0530

    update admin route

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -7,12 +7,4 @@ const { adminProtect } = require("../middlewares/authMiddleware");
 // Admin Login Route
 router.post("/login", adminLogin);
 
-// Example of a protected route
-router.get("/dashboard", adminProtect, (req, res) => {
-  res.status(200).json({
-    message: "Welcome to admin dashboard",
-    admin: req.admin,
-  });
-});
-
 module.exports = router;
